fc13cb42b5c419a62c8340b5db47dcb3ec35ff4c
markSancho7 <marcos.sancho7@alumnostrazos.net>
2024-03-07 12:33:35 +0100

    actual

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
+import { useEffect } from 'react';
 import { GlobalStyles } from './styles/GlobalStyles';
 
 const App = () => {
+	useEffect(() => {
+		fetchData();
+	});
 	return (
 		<>
 			<GlobalStyles />
-			<h1>REACT OK</h1>
 		</>
 	);
 };
 
+const fetchData = async () => {
+	const response = await fetch(url);
+	const data = await response.json();
+
+	console.log(data);
+};
 export default App;
